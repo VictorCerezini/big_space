de30547cce210c571e1c5a9c161ce2d60fa7e52e
VictorCerezini <victorcerezini@hotmail.com>
2026-07-09 01:14:53 -0300

    Upgraded to Bevy 0.19.

--- examples/spatial_hash.rs
+++ examples/spatial_hash.rs
@@ -1,8 +1,8 @@
 //! Demonstrates the included optional spatial hashing and partitioning of grid cells.
 
 use bevy::{
-    core_pipeline::tonemapping::Tonemapping, post_process::bloom::Bloom, prelude::*,
-    render::view::Hdr, window::CursorOptions,
+    camera::Hdr, core_pipeline::tonemapping::Tonemapping, post_process::bloom::Bloom,
+    prelude::*, window::CursorOptions,
 };
 use bevy_ecs::entity::EntityHasher;
 use bevy_math::DVec3;
@@ -362,8 +362,8 @@ fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
             parent.spawn((
                 Text::default(),
                 TextFont {
-                    font: asset_server.load("fonts/FiraMono-Regular.ttf"),
-                    font_size: 14.0,
+                    font: asset_server.load("fonts/FiraMono-Regular.ttf").into(),
+                    font_size: FontSize::Px(14.0),
                     ..default()
                 },
             ));
